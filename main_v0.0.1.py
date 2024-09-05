9fef664c0894fdc24ea7e38a1a1fc0731a5df6d3
JS3322 <42951920+JS3322@users.noreply.github.com>
2024-09-05 11:49:25 +0900

    main_v0.0.1.py 업데이트

--- main_v0.0.1.py
+++ main_v0.0.1.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from src.mlops.application import ExecuteExampleApplication
 
 import uvicorn
 
 app = FastAPI()
+
+app.include_router(ExecuteExampleApplication.router, prefix="/v1/autotdk/publish/execute")
 
 if __name__ == "__main__"
 	uvicorn.run(app, host="0.0.0.0", port=8000)
