94248fd670e1dc7b8a6bcb3b5ce046d6afd94e0f
js3322 <js@cleancode.kr>
2024-09-07 21:38:55 +0900

    fix script

--- main_v0.0.1_start_script.sh
+++ main_v0.0.1_start_script.sh
@@ -1,22 +1,24 @@
 #!/bin/csh
 
-# Check the current directory
+set execute_pyfile_name = 'main_v0.0.1'
+
+# check the current directory
 set current_dir = `pwd`
-echo "Current directory: $current_dir"
+echo "current directory: $current_dir"
 
 # Install Python virtual environment (create it if the venv directory does not exist)
 if (! -d "$current_dir/venv") then
-    echo "Creating virtual environment."
+    echo "creating virtual environment."
     python -m venv "$current_dir/venv"
 endif
 
 # Activate the virtual environment
 source "$current_dir/venv/bin/activate.csh"
-echo "Virtual environment activated."
+echo "virtual environment activated."
 
 # Check if requirements.txt exists and install the libraries
 if (-f "$current_dir/requirements.txt") then
-    echo "Installing libraries from requirements.txt."
+    echo "installing libraries from requirements.txt."
     pip install -r "$current_dir/requirements.txt"
 else
     echo "requirements.txt file not found."
@@ -24,10 +26,10 @@ endif
 
 # Run main_v2.py (Python logs will be output to the terminal)
 if (-f "$current_dir/main_v2.py") then
-    echo "Running main_v2.py."
-    python "$current_dir/main_v2.py"
+    echo "running main_v2.py."
+    python "$current_dir/$execute_pyfile_name.py"
 else
-    echo "main_v2.py file not found."
+    echo "$execute_pyfile_name.py file not found."
 endif
 
 # Deactivate the virtual environment
