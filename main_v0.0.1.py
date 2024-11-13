e7b9c25f8bb0f65f14744fa4e0b6ecf1582c5d6b
js3322 <js@cleancode.kr>
2024-11-13 21:18:11 +0900

    create html

--- main_v0.0.1.py
+++ main_v0.0.1.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, Depends, Cookie
+from fastapi import FastAPI, Depends, Cookie, HTTPException
+from fastapi.responses import HTMLResponse
 from src.common.middleware.example import add_process_time_header
 from src.ml.application import execute_example_application
 
 import uvicorn
+import os
 
 app = FastAPI(
 	title="mlops API interface",
@@ -19,6 +21,20 @@ async def get_token_from_cookie(token: str = Cookie(None)):
 @app.get("/token")
 async def read_token(token: str = Depends(get_token_from_cookie)):
     return {"token": token}
+
+@app.get("/intro", response_class=HTMLResponse)
+async def intro_page(token: str = Depends(get_token_from_cookie), bbb: str = "dddd"):
+    if token:
+        # HTML 파일 경로
+        file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "src/static/intro.html")
+        with open(file_path, "r") as file:
+            html_content = file.read()
+            # bbb 값을 HTML에 삽입
+            html_content = html_content.replace("{{ bbb_value }}", bbb)
+            return html_content
+    else:
+        # 토큰이 없을 경우 500 에러 발생
+        raise HTTPException(status_code=500, detail="Internal Server Error: Token not found.")
 
 app.middleware("http")(add_process_time_header)
 app.include_router(execute_example_application.route, prefix="/v1/ml/request")
